ca13a19fa03af9e24951fd609690a18d4cb99562
zyy2011 <vyvn2011@gmail.com>
2024-10-21 00:12:30 +0700

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Bám Tiếp Bên Dưới Nha'.split('')
+const text = 'Bấm Tiếp Bên Dưới Nha'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
